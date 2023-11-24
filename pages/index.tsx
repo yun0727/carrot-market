8c05d003386c4471631c1416434535abde867c56
yun <austru00@gmail.com>
2023-11-24 21:15:00 +0900

    4.5 Transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,9 +58,9 @@ const Home: NextPage = () => {
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
             <div className="space-x-2">
-              <button className="w-5 h-5 rounded-full bg-yellow-500"/>
-              <button className="w-5 h-5 rounded-full bg-indigo-500"/>
-              <button className="w-5 h-5 rounded-full bg-teal-500"/>
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 transition ring-yellow-500"/>
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 transition ring-indigo-500"/>
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 transition ring-teal-500"/>
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
