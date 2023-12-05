54e4182fd66a8c9458f9d6672039c039965dd107
yun <austru00@gmail.com>
2023-12-05 19:59:06 +0900

    5.19 Conclusions

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";  
+import Link from "next/link";
 import Layout from "@/components/layout";
 
 const Chats: NextPage=()=>{
@@ -6,10 +7,8 @@ const Chats: NextPage=()=>{
     <Layout hasTabBar title="채팅">
       <div className="divide-y-[1px] ">
         {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <div
-            key={i}
-            className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-          >
+          <Link legacyBehavior href={`/chats/${i}`} key={i}>
+          <a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
               <p className="text-gray-700">Steve Jebs</p>
@@ -17,7 +16,8 @@ const Chats: NextPage=()=>{
                 See you tomorrow in the corner at 2pm!
               </p>
             </div>
-    </div>
+          </a>
+        </Link>
     ))}
     </div>
   </Layout>
