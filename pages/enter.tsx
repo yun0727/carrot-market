f7e1b31add4d040d037fbe4fb0269ef0ba7c9370
yun <austru00@gmail.com>
2023-12-09 16:56:03 +0900

    8.2 Clean Code 1

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next";
 import { useState } from "react";
-import { cls } from "@/libs/utils";
+import { cls } from "@/libs/client/utils";
 import Button from "../components/button";
 import Input from "../components/input";
 import { useForm } from "react-hook-form";
+import useMutation from "@/libs/client/useMutation";
 
 interface EnterForm {
   email?: string;
@@ -11,6 +12,7 @@ interface EnterForm {
 }
 
 const Enter: NextPage = () => {
+  const [enter, { loading, data, error }] = useMutation("/api/users/enter");
   const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
@@ -23,16 +25,7 @@ const Enter: NextPage = () => {
     setMethod("phone");
   };
   const onValid = (data: EnterForm) => {
-    setSubmitting(true);
-    fetch("/api/users/enter", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application.json",
-      },
-    }).then(() => {
-      setSubmitting(false);
-    });
+    enter(data);
   };
   return (
     <div className="mt-16 px-4">
