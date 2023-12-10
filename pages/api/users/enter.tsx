2e6d94175476e9eeb11ec50bea71d2c33f22f267
yun <austru00@gmail.com>
2023-12-10 14:02:18 +0900

    8.4 withHandler

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,12 +1,9 @@
+import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.status(200).end();
+  return res.status(200).end();
 }
+
+export default withHandler("POST", handler);
