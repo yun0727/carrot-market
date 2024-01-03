cb4cf81e4a7402097afda7cfe3bd68f8aa5dbc00
yun <austru00@gmail.com>
2024-01-03 20:19:19 +0900

    9.5 Sending Email(nodemailer이용)

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,6 +2,7 @@ import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import smtpTransport from "@libs/server/email";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -37,6 +38,29 @@ async function handler(
     });
     console.log(message);
   }
+  if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "Nomad Carrot Authentication Email",
+      text: `Authentication Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
+  }
+
   return res.json({
     ok: true,
   });
