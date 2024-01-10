e23520470e165a65a828c2be873b9327c6a46967
yun <austru00@gmail.com>
2024-01-10 22:34:42 +0900

    13.3 Profile Page

--- pages/api/reviews.ts
+++ pages/api/reviews.ts
@@ -6,25 +6,25 @@ import { withApiSession } from "@libs/server/withSession";
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
-){
-  const{
-    session: {user},
-  }=req;
+) {
+  const {
+    session: { user },
+  } = req;
   const reviews = await client.review.findMany({
-    where:{
-      createdForId:user?.id,
+    where: {
+      createdForId: user?.id,
     },
-    include:{createdBy:{select:{id: true, name:true, avatar:true,}}}
+    include: { createdBy: { select: { id: true, name: true, avatar: true } } },
   });
   res.json({
     ok: true,
     reviews,
-  })
+  });
 }
 
 export default withApiSession(
   withHandler({
-    methods:["GET"],
-    handler
+    methods: ["GET"],
+    handler,
   })
-)
+);
