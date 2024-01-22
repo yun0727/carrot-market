ebf88fdffca3de80d8f12910066f92e1196b8fbe
yun <austru00@gmail.com>
2024-01-22 16:33:18 +0900

    15.8 Product Images

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -22,7 +22,7 @@ async function handler(
   }
   if (req.method === "POST"){
     const{
-      body:{name, price, description},
+      body:{name, price, description, photoId},
       session:{user},
     }=req;
     const product = await client.product.create({
@@ -30,7 +30,7 @@ async function handler(
         name,
         price:+price,
         description,
-        image:"xx",
+        image:photoId,
         user:{
           connect:{
             id: user?.id
