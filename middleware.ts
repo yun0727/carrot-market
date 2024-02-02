4b3efdf23ccead35c8dedf1b5df3fd93964f214f
yun <austru00@gmail.com>
2024-02-02 19:02:24 +0900

    19.1 Responses and Redirections

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,20 @@
-import { NextFetchEvent, NextRequest } from "next/server";
+import { getIronSession } from "iron-session";
+import { NextFetchEvent, NextRequest, NextResponse, userAgent, } from "next/server";
 
-export function middleware(req: NextRequest, ev: NextFetchEvent){
-  console.log("hello")
+export const middleware = async(req: NextRequest, ev: NextFetchEvent)=>{
+  if (userAgent(req).isBot){
+    return new Response("Plz don't be a bot. Be human", {status: 403})
+  }
+  if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
+    req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+    req.nextUrl.pathname = "/enter";
+    return NextResponse.redirect(req.nextUrl);
+    }
   if (req.nextUrl.pathname.startsWith('/chats')){
     console.log(`this is chats ONLY middleware`)
   }
+}
+
+export const config = {
+  mather: ["/((?!api|_next/static|favicon.ico).*)"],
 }
