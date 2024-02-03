eec4298a743f81b41fc38ed1c186eaba996ae2b9
yun <austru00@gmail.com>
2024-02-03 20:14:12 +0900

    19.8 SSR + Authentication

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,4 +1,4 @@
-import { withIronSessionApiRoute } from "iron-session/next";
+import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
 
 declare module "iron-session"{
   interface IronSessionData{
@@ -16,4 +16,8 @@ const cookieOptions={
 
 export function withApiSession(fn:any){
   return withIronSessionApiRoute(fn, cookieOptions)
+}
+
+export function withSsrSession(handler:any){
+  return withIronSessionSsr(handler, cookieOptions)
 }
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,11 +1,13 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Layout from "@components/layout";
 import Link from "next/link";
 import { Review, User } from "@prisma/client";
 import useUser from "@libs/client/useUser";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { cls } from "@libs/client/utils";
 import Image from "next/image";
+import { withSsrSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -175,4 +177,30 @@ const Profile: NextPage = () => {
   );
 };
 
-export default Profile;
+const Page: NextPage<{ profile: User }> = ({ profile }) => {
+  return (
+    <SWRConfig
+      value={{
+        fallback: {
+          "/api/users/me": { ok: true, profile },
+        },
+      }}
+    >
+      <Profile />
+    </SWRConfig>
+  );
+};
+
+export const getServerSideProps = withSsrSession(async function ({
+  req,
+}: NextPageContext) {
+  const profile = await client.user.findUnique({
+    where: { id: req?.session.user?.id },
+  });
+  return {
+    props: {
+      profile: JSON.parse(JSON.stringify(profile)),
+    },
+  };
+});
+export default Page;
