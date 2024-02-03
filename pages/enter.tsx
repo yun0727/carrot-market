613083ccc5a844b933a0e66e56f133dbbaeaea91
yun <austru00@gmail.com>
2024-02-03 17:35:23 +0900

    19.3 Lazy-load Imports

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Button from "@components/button";
 import Input from "@components/input";
@@ -8,7 +8,13 @@ import { cls } from "@libs/client/utils";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 
-const Bs = dynamic(() => import("@components/bs"), { ssr: false });
+const Bs = dynamic(
+  () =>
+    new Promise((resolve) =>
+      setTimeout(() => resolve(import("@components/bs")), 10000)
+    ),
+  { ssr: false, suspense: true, loading: () => <span>loading</span> }
+);
 
 interface EnterForm {
   email?: string;
@@ -119,7 +125,9 @@ const Enter: NextPage = () => {
               ) : null}
               {method === "phone" ? (
                 <>
-                  <Bs />
+                  <Suspense fallback={<button>loading!!</button>}>
+                    <Bs />
+                  </Suspense>
                   <Input
                     register={register("phone")}
                     name="phone"
