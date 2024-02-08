5afc9631c24d89629c63b0408a95030c65d2bd38
yun <austru00@gmail.com>
2024-02-08 16:21:25 +0900

    21.4 Parallelism

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack5: true, webpack: (config, options) => { config.cache = false; return config; },
-  experimental: {
-    reactRoot: true, 
-    runtime: "node.js", 
-    serverComponents:true,
-  },
+  // webpack5: true, webpack: (config, options) => { config.cache = false; return config; },
+  // experimental: {
+  //   reactRoot: true, 
+  //   runtime: "node.js", 
+  //   serverComponents:true,
+  // },
   images:{
     domains :["imagedelivery.net", "videodelivery.net"]
   }
