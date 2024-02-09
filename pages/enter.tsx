2d27e70e4e91dcf5aed7af27640791ff3ccba4fd
yun <austru00@gmail.com>
2024-02-09 18:38:57 +0900

    New Welcome Msg

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -60,7 +60,9 @@ const Enter: NextPage = () => {
   }, [tokenData, router]);
   return (
     <div className="mt-16 px-4">
-      <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+      <h3 className="text-3xl font-bold text-center">
+        Welcome to my Carrot Baby
+      </h3>
       <div className="mt-12">
         {data?.ok ? (
           <form
